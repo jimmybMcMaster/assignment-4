b1e47b196a0083f92dc88e66cdfc14e5c4e7b41c
James Barbour <jameskbarbour1@gmail.com>
2025-06-04 16:19:26 +0000

    test push

--- database_access.ts
+++ database_access.ts
@@ -10,3 +10,5 @@ const uri = 'mongodb://mongo'
 export const client = new MongoClient(uri)
 export const database = client.db('mcmasterful-books')
 export const bookCollection = database.collection<Book>('books')
+
+//TEST PUSH
